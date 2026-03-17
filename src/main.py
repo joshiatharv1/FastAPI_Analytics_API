cc08b7c2d954761e1e4e7f23d49cb15f5a071c54
Atharv Joshi <joshi.atharv1@northeastern.edu>
2026-03-16 21:07:46 -0400

    Basid DataTypes in Python

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
 from typing import Union
+from api.events import router as event_router
 app=FastAPI()
+
+app.include_router(event_router, prefix='/api/event')
 
 @app.get("/")
 def read_root():
@@ -9,3 +12,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q:Union[str, None]=None):
     return {"item_id": item_id, "q":q}
+
+@app.get("/healthz")
+def read_api_health():
+    return {"status": "ok"}
